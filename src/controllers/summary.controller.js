6464b8bc6c41fe1a981c917750ca91470c8a7a31
geraldSolanomelt <gerald.macias@meltstudio.co>
2023-09-17 18:00:59 -0600

    feat: adding summary objects to graphs

--- src/controllers/summary.controller.js
+++ src/controllers/summary.controller.js
@@ -1,5 +1,7 @@
 import Transactions from "../models/transaction";
 import Types from "../models/type";
+import { fn, col } from "sequelize";
+import Category from "../models/category";
 
 export const getSummary = async (req, res) => {
   try {
@@ -21,12 +23,42 @@ export const getSummary = async (req, res) => {
       include: [{ model: Types, as: "transactionType" }],
     });
 
+    const expensesByCategory = await Transactions.findAll({
+      where: { "$transactionType.id$": expenseType.id },
+      attributes: [[fn("SUM", col("amount")), "total"]],
+      include: [
+        { model: Types, as: "transactionType" },
+        {
+          model: Category,
+          as: "transactionCategory",
+          attributes: ["name"],
+        },
+      ],
+      group: ["transactionCategory.id"],
+    });
+
+    const incomesByCategory = await Transactions.findAll({
+      where: { "$transactionType.id$": incomeType.id },
+      attributes: [[fn("SUM", col("amount")), "total"]],
+      include: [
+        { model: Types, as: "transactionType" },
+        {
+          model: Category,
+          as: "transactionCategory",
+          attributes: ["name"],
+        },
+      ],
+      group: ["transactionCategory.id"],
+    });
+
     const balance = totalIncome - totalExpense;
 
     return res.status(200).json({
       totalIncome,
       totalExpense,
       balance,
+      expensesByCategory,
+      incomesByCategory,
     });
   } catch (error) {
     return res.status(500).json({ error: error.message });
